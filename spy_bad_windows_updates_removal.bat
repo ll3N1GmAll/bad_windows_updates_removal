e95dd49bd3317c9a16aea5d46f8e6fe442b72da5
David Vaughan <e777a63f@opayq.com>
2017-04-26 12:41:31 -0500

    spy_bad_windows_updates_removal.bat

---
+++ spy_bad_windows_updates_removal.bat
@@ -0,0 +1,26 @@
+@echo off
+wusa /uninstall /kb:3075249 /quiet /norestart
+wusa /uninstall /kb:3080149 /quiet /norestart
+wusa /uninstall /kb:2876229 /quiet /norestart
+wusa /uninstall /kb:2923545 /quiet /norestart
+wusa /uninstall /kb:2970228 /quiet /norestart
+wusa /uninstall /kb:3035583 /quiet /norestart
+wusa /uninstall /kb:2990214 /quiet /norestart
+wusa /uninstall /kb:3021917 /quiet /norestart
+wusa /uninstall /kb:3068708 /quiet /norestart
+wusa /uninstall /kb:2592687 /quiet /norestart
+wusa /uninstall /kb:2660075 /quiet /norestart
+wusa /uninstall /kb:2506928 /quiet /norestart
+wusa /uninstall /kb:2952664 /quiet /norestart
+wusa /uninstall /kb:3050265 /quiet /norestart
+wusa /uninstall /kb:2726535 /quiet /norestart
+wusa /uninstall /kb:2994023 /quiet /norestart
+wusa /uninstall /kb:3022345 /quiet /norestart
+wusa /uninstall /kb:3068708 /quiet /norestart
+wusa /uninstall /kb:3022345 /quiet /norestart
+wusa /uninstall /kb:2545698 /quiet /norestart
+wusa /uninstall /kb:3065987 /quiet /norestart
+wusa /uninstall /kb:3068708 /quiet /norestart
+wusa /uninstall /kb:3075249 /quiet /norestart
+wusa /uninstall /kb:3080149 /quiet /norestart
+END
