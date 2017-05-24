f24c6d951d9a5669470c51750ae1c09a9723be94
David Vaughan <e777a63f@opayq.com>
2017-05-23 21:55:18 -0500

    Update spy_bad_windows_updates_removal.bat

--- spy_bad_windows_updates_removal.bat
+++ spy_bad_windows_updates_removal.bat
@@ -23,3 +23,4 @@ wusa /uninstall /kb:3065987 /quiet /norestart
 wusa /uninstall /kb:3068708 /quiet /norestart
 wusa /uninstall /kb:3075249 /quiet /norestart
 wusa /uninstall /kb:3080149 /quiet /norestart
+exit
