af588b9ae19c04cb7c1c3e03cec8f79222d36a63
ll3N1GmAll <e777a63f@opayq.com>
2017-11-23 23:03:25 -0600

    Update spy_bad_windows_updates_removal.bat
    Script now lets you know which updates were installed and asks you for confirmation before the update is removed so you can know if any were present; and so you can be sure the script is functioning as expected.

--- spy_bad_windows_updates_removal.bat
+++ spy_bad_windows_updates_removal.bat
@@ -1,26 +1,25 @@
-@echo off
-wusa /uninstall /kb:3075249 /quiet /norestart
-wusa /uninstall /kb:3080149 /quiet /norestart
-wusa /uninstall /kb:2876229 /quiet /norestart
-wusa /uninstall /kb:2923545 /quiet /norestart
-wusa /uninstall /kb:2970228 /quiet /norestart
-wusa /uninstall /kb:3035583 /quiet /norestart
-wusa /uninstall /kb:2990214 /quiet /norestart
-wusa /uninstall /kb:3021917 /quiet /norestart
-wusa /uninstall /kb:3068708 /quiet /norestart
-wusa /uninstall /kb:2592687 /quiet /norestart
-wusa /uninstall /kb:2660075 /quiet /norestart
-wusa /uninstall /kb:2506928 /quiet /norestart
-wusa /uninstall /kb:2952664 /quiet /norestart
-wusa /uninstall /kb:3050265 /quiet /norestart
-wusa /uninstall /kb:2726535 /quiet /norestart
-wusa /uninstall /kb:2994023 /quiet /norestart
-wusa /uninstall /kb:3022345 /quiet /norestart
-wusa /uninstall /kb:3068708 /quiet /norestart
-wusa /uninstall /kb:3022345 /quiet /norestart
-wusa /uninstall /kb:2545698 /quiet /norestart
-wusa /uninstall /kb:3065987 /quiet /norestart
-wusa /uninstall /kb:3068708 /quiet /norestart
-wusa /uninstall /kb:3075249 /quiet /norestart
-wusa /uninstall /kb:3080149 /quiet /norestart
-exit
+wusa /uninstall /kb:3075249 /norestart
+wusa /uninstall /kb:3080149 /norestart
+wusa /uninstall /kb:2876229 /norestart
+wusa /uninstall /kb:2923545 /norestart
+wusa /uninstall /kb:2970228 /norestart
+wusa /uninstall /kb:3035583 /norestart
+wusa /uninstall /kb:2990214 /norestart
+wusa /uninstall /kb:3021917 /norestart
+wusa /uninstall /kb:3068708 /norestart
+wusa /uninstall /kb:2592687 /norestart
+wusa /uninstall /kb:2660075 /norestart
+wusa /uninstall /kb:2506928 /norestart
+wusa /uninstall /kb:2952664 /norestart
+wusa /uninstall /kb:3050265 /norestart
+wusa /uninstall /kb:2726535 /norestart
+wusa /uninstall /kb:2994023 /norestart
+wusa /uninstall /kb:3022345 /norestart
+wusa /uninstall /kb:3068708 /norestart
+wusa /uninstall /kb:3022345 /norestart
+wusa /uninstall /kb:2545698 /norestart
+wusa /uninstall /kb:3065987 /norestart
+wusa /uninstall /kb:3068708 /norestart
+wusa /uninstall /kb:3075249 /norestart
+wusa /uninstall /kb:3080149 /norestart
+END
